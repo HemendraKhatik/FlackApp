9ccd4dbff69c89a76a51ec450b2bdd34565a9595
AamChora <37813372+HemendraKhatik@users.noreply.github.com>
2019-05-04 23:09:24 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,15 @@ function setName() {
     const name =document.querySelector('#name').value;
     localStorage.setItem('name', name);
     document.querySelector('#nama').setAttribute("disabled", "disabled"); 
-}                          
+} 
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}                         
 document.addEventListener('DOMContentLoaded', () => {
 
     // Connect to websocket
@@ -10,24 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
      
     // Setting user connection in room
     socket.on('connect', () => {
-        socket.emit('connection',{'msg':'One Anonymous joined the room'});   
-    });
-    // When a new user connected to the room
-    socket.on('connection successful', data => { 
-        const p = document.createElement('p');
-        p.innerHTML = `<b>${data.user}:</b>`;
-        document.querySelector('#message').append(p);
-        const hr = document.createElement('hr');
-        document.querySelector('#message').append(hr);
-
-        shouldScroll = message.scrollTop + message.clientHeight === message.scrollHeight;
-        if (!shouldScroll) {
-            scrollToBottom();
-        }
-    });
-    //checking local storage valiable           
+        //checking local storage valiable           
         if (!localStorage.getItem('name'))
             localStorage.setItem('name','Anonymous'); 
+        // When someone entered in room
+          $("#enter").click(function(){
+          $("#pop").hide();
+          $('#send').removeAttr("disabled");
+          //should emit a "submit message" event
+          name = localStorage.getItem('name');
+          const message ="Entered in room"           
+          const rooma = document.querySelector('#room').innerHTML;
+          socket.emit('submit message', {'message': message,'name':name, 'rooma':rooma});
+        });
+      
         //should emit a "submit message" event
         document.querySelector('#send').onclick = () => {
             name = localStorage.getItem('name');
@@ -56,5 +60,5 @@ document.addEventListener('DOMContentLoaded', () => {
     function scrollToBottom() {
     message.scrollTop = message.scrollHeight;
     }
-   
+  });  
 });
