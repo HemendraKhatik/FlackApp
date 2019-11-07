b87258aa1fa9963974056869812cb7bd403fa473
Kamran Hayati <kaloneh@gmail.com>
2019-11-07 07:59:10 +0330

    Merge flack-master and flack-kaloneh-master (#1)
    * Add responsiveness to sign up and login pages.
    
    * Delete 2029240f6d1128be89ddc32729463129
    
    * Delete 6df741a221b49fc83abd09602f2efa63
    
    * Delete application.cpython-36.pyc
    
    * Delete encryption.cpython-36.pyc
    
    * Adding encryption.py to encrypt password and messages for security reasons
    
    * Adding some comment on encryption.py and application.py
    
    * Adding ignorable files to .gitignore
    
    * Adding ignorable files to .gitignore
    
    * fixed email validation issue in signup form.
    
    * new feature added - passwords are now encrypted
    
    Now the user's security has increased. Not even the developer has access to your account due to the encrypted password.
    
    * Delete ui.html
    
    deleted this because this file was created for test purposes.
    
    * Delete profile.html
    
    deleted this because this file was created for test purposes.
    
    * fix database hardcoded url
    
    * make easier to focus on login form inputs
    
    * Delete os.py
    
    deleted this for test purposes.
    
    * Delete functools.py
    
    deleted this for test purposes.
    
    * Add responsive 'Create Room' modal.
    
    * Style create room button.
    
    * Add create-room class to button.
    
    * Style chatroom messages.
    
    * Adjust alignment on 'Create Room.'
    
    * Style chatroom popup.
    
    * Clean Code
    
    * clean code
    
    * login validation issue fixed
    
    * refactor
    
    * Update application.py
    
    * Update application.py
    
    * fix time issue in chatroom
    
    * email validation or password validation
    
    * Update signup.html
    
    * Update application.py
    
    * Remove unnecessary code
    
    * base.html created
    
    * Update chatroom.html
    
    * Update login.html
    
    * Update signup.html
    
    * email varification added
    
    * Update signup.html
    
    * Update application.py
    
    * Update login.html
    
    * Update application.py
    
    * Update application.py
    
    * Update application.py
    
    * Made title blocks more consistent in capitalization
    
    * Added comments indicating beginning of flask initialization and route definitions
    
    * Organizing Backend Layer
    
    * Modifying Indentation of README.md
    
    * Organizing Backend Layer
    
    * Organizing Backend Layer
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Adding AppRunner Scripts (runapp.ps1 & runapp.cmd)
    
    * Updating runapp.ps1 and runapp.cmd

--- application.py
+++ application.py
@@ -1,13 +1,24 @@
 import os
+import re
+
 from flask import Flask, session, render_template, request, url_for, redirect, flash, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask_socketio import SocketIO, emit ,join_room, send
-from functools import wraps # for security purpose
+from flask_socketio import SocketIO, emit, join_room, send
+from functools import wraps  # for security purpose
+
+from models.channel import ChannelDAO, Channel
+from models.exceptions import WordSizeException
+from models.user import UserDAO, User
+from util.encryption import *
+from util.form_validation import UserFieldValidation
+
+
+"""Start of flask app initialization"""
 
 app = Flask(__name__)
-DATABASE_URL="postgres://vfobhheluegnpw:0704135dad9d809b773c6ad4555bfdc87cd76999ccb90c8c99a0ec982f3267de@ec2-23-21-130-182.compute-1.amazonaws.com:5432/da71gb54aqbou7"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
@@ -25,144 +36,261 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+user_dao = UserDAO(db)
+channel_dao = ChannelDAO(db)
+
+
+
+def setup_database():
+    global engine
+    global db
+    if engine == None:
+        engine = create_engine(os.getenv("DATABASE_URL"))
+    if db == None:
+        db = scoped_session(sessionmaker(bind=engine))
+
+
+setup_database()
+
+# Instantiating encryption util
+psw_hasher = HashTable('md5')
+msg_hasher = HashTable('sha1')
+
+"""Route Definitions"""
+
 @app.route("/index")
 def index():
-	if request.method == "GET":
-			if 'logged_in' in session:
-				return redirect(url_for('home')) 
-	return render_template("login.html")
+    if request.method == "GET":
+        if 'logged_in' in session:
+            return redirect(url_for('home'))
+    return render_template("login.html")
 
+
+#This is your base route
 @app.route("/")
 def welcome():
-	if request.method == "GET":
-			if 'logged_in' in session:
-				return redirect(url_for('home')) 
-	return render_template("login.html")	
+    if request.method == "GET":
+        if 'logged_in' in session:
+            return redirect(url_for('home'))
+    return render_template("login.html")
 
-@app.route("/signup",methods=["POST","GET"])
+
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
-	if request.method=="GET":
-		return render_template('signup.html')
-	username=request.form.get("username")
-	email=request.form.get("email")
-	if request.form.get("password") == request.form.get("c_password"):
-		password=request.form.get("password")
-	else:
-		flash('Password does not match')
-		return redirect(url_for('index'))
-	db.execute("INSERT INTO user_signup_data(username,email,password) VALUES(:username,:email,:password)",
-	{"username":username,"email":email,"password":password})
-	db.commit()
-	db.close()
-	return render_template('login.html')
+    # In idle database loses its connection and should has been refreshed
+    setup_database()
+    if request.method == "GET":
+        return render_template('signup.html', error_visibility='none')
+    username = request.form.get("username")
 
-@app.route("/login",methods=["POST","GET"])
+    email = request.form.get("email")
+    email_error_msg = form_check_email(email)
+    if email_error_msg is not '':
+        flash(email_error_msg, 'error')
+        return render_template('signup.html', error_visibility='block', error_msg=email_error_msg)
+
+    if request.form.get("password") == request.form.get("c_password"):
+        # checking password strength 
+        password_strength = form_password_strength(request.form.get("password"))
+        if password_strength == "weak password":
+            flash(password_strength, 'error')
+            return redirect(request.url)
+        if password_strength == "medium password":
+            flash(password_strength, 'error')
+            return redirect(request.url)
+        # The password encryption is satisfied once the user signs up in the right way so that user_dao.saveUser
+        # is responsible for encryption. Double encryption is tricky and must be avoided.
+        password = request.form.get("password")
+    else:
+        error_msg = 'Password does not match'
+        flash(error_msg, 'error')
+        return render_template('signup.html', error_visibility='block')
+    user = User(username=username, email=email, password=password)
+    # The password will be encrypted here and double encryption must be avoided!
+    user_dao.saveUser(user, encryptPassword=True)
+    # db.execute("INSERT INTO user_signup_data(username,email,password) VALUES(:username,:email,:password)",
+    #            {"username": username, "email": email, "password": password})
+    # db.commit()
+    # db.close()
+    return render_template('login.html')
+
+
+def form_check_email(email):
+    error_msg = ''
+    user = user_dao.findUserByEmail(email)
+    # user = db.execute("SELECT username FROM user_signup_data WHERE email=:email", {"email": email}).fetchone()
+    if user:
+        error_msg += 'Email already exists!'
+    else:
+        match = re.search(r'[\w.-]+@[\w.-]+.\w+', email)
+        if match is None:
+            error_msg += 'Email not valid!'
+    return error_msg
+
+def form_password_strength(password):
+    valid_count = 0
+    if len(password) >= 8:
+        valid_count += 1
+    if re.search(r'[A-Z]+', password):
+        valid_count += 1
+    if re.search(r'[a-z]+', password):
+        valid_count += 1
+    if re.search(r'[$-/:-?{-~!"^_`\[\]]+', password):
+        valid_count += 1
+    if valid_count == 4:
+        return "strong password"
+    elif valid_count == 3:
+        return "medium password"
+    return "weak password"
+
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
-	#This route will only accept the POST request
-	if request.method == "POST":
-		username=request.form.get("username")
-		password=request.form.get("password")
-		query=db.execute("SELECT * FROM user_signup_data WHERE username=:username AND password=:password",
-		{"username":username,"password":password}).fetchall()
-		"""Lists all channels."""
-		channels = db.execute("SELECT * FROM user_channel").fetchall()
-		for q in query:
-			if q.username==username and q.password==password:
-				"""Using session here to keep all users sessions separate from each other"""
-				session['logged_in'] = True
-				session['username'] = q.username
-				session['user_id'] = q.id
-				return redirect(url_for('home'))
-	if request.method == "GET":
-		if 'logged_in' in session:
-			redirect(url_for('home'))
-	return redirect(url_for('index'))
+    # In idle database loses its connection and should has been refreshed
+    setup_database()
+    # This route will only accept the POST request
+    if request.method == "POST":
+        username = request.form.get("username")
+        # For now, the plain text is gonna be encrypted easily; the better way is considering encryption
+        # from the begining overall sessions, requests, even Ajax requests, etc.
+        password = psw_hasher.hexdigest(request.form.get("password"))
+        user = None
+        """To ensure that user can log in with either of username and password"""
+        try:
+            if UserFieldValidation().validateEmail(username):
+                user = user_dao.findUserByEmail(username)
+            else:
+                user = user_dao.findUserByUsername(username)
+        except WordSizeException as e:
+            user = user_dao.findUserByUsername(username)
+        # user_exists = db.execute("SELECT username from user_signup_data WHERE username=:username",
+        #                          {"username": username}).fetchall()
+        if user is None:
+            flash('Account does not exist')
+            return redirect(url_for('index'))
+        elif password == user.password:
+            """Using session here to keep all users sessions separate from each other"""
+            session['logged_in'] = True
+            session['username'] = user.username
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+        flash("Invalid password")
+    elif request.method == "GET":
+        """:session still can have ``logged_in`` with the value that is not necessarily True,
+         e.g., False, '' as an empty string, etc., so that checking session to contain a key is not enough!"""
+        if 'logged_in' in session :
+            redirect(url_for('home'))
+    return redirect(url_for('index'))
+
 
 @app.route("/logout")
 def logout():
-	session.pop('logged_in',None)
-	return redirect(url_for('index'))
+    if 'logged_in' in session:
+        session.pop('logged_in', None)
+        session.clear()
+    return redirect(url_for('index'))
 
-@app.route("/home",methods=["POST","GET"]) 
+
+@app.route("/home", methods=["POST", "GET"])
 def home():
-	if request.method == "POST":
-		channels = db.execute("SELECT * FROM user_channel").fetchall()
-		return render_template("chatroom.html",user_id=session['user_id'],user_name=session['username'],channels=channels)
-	else:
-		if request.method == "GET":
-			if 'logged_in' in session:
-				"""Need to have some variables to pass"""
-				return redirect(url_for('channels')) 
-			flash('Need to login')
-			return redirect(url_for('index'))
+    if request.method == "POST":
+        # In idle database loses its connection and should has been refreshed
+        setup_database()
+        channels = channel_dao.findall()
+        # channels = db.execute("SELECT * FROM user_channel").fetchall()
+        return render_template(
+            "chatroom.html", user_id=session['user_id'], user_name=session['username'],channels=channels
+        )
+    else:
+        if request.method == "GET":
+            if 'logged_in' in session:
+                """Needs to have some variables to pass"""
+                return redirect(url_for('channels'))
+            flash('Login is required')
+            return redirect(url_for('index'))
+
 
 """Securing direct get methods"""
-def login_required(test):
-	@wraps(test)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return test(*args, **kwargs)
-		else:
-			flash('you need to login first')
-			return redirect(url_for('index'))
-	return wrap
 
-@app.route("/channel_creation",methods=["POST"])
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('you need to login first')
+            return redirect(url_for('index'))
+
+    return wrap
+
+
+@app.route("/channel_creation", methods=["POST"])
 @login_required
 def channel_creation():
-	channel=request.form.get("channel")
-	description=request.form.get("description")
-	u_id=request.form.get("u_id")
-	db.execute("INSERT INTO user_channel(channel,description,u_id) VALUES(:channel,:description,:u_id)",
-	{"channel":channel,"description":description,"u_id":u_id})
-	db.commit()
-	db.close()			
-	return redirect(url_for('channels'))
+    channel = request.form.get("channel")
+    description = request.form.get("description")
+    u_id = request.form.get("u_id")
+    channel_dao.saveChannel(Channel(channel=channel, description=description, u_id=u_id))
+    # db.execute("INSERT INTO user_channel(channel,description,u_id) VALUES(:channel,:description,:u_id)",
+    #            {"channel": channel, "description": description, "u_id": u_id})
+    # db.commit()
+    # db.close()
+    return redirect(url_for('channels'))
+
 
 @app.route("/channels")
 @login_required
 def channels():
-	"""Lists all channels."""
-	global channels
-	channels = db.execute("SELECT * FROM user_channel").fetchall()
-	flack="Flack"
-	channel_decription ="This room is flack official public room"
-	return render_template("chatroom.html",flack=flack,user_id=session['user_id'],user_name=session['username'], channels=channels,channel_decription=channel_decription)
+    """Lists all channels."""
+    channels = channel_dao.findall()
+    # channels = db.execute("SELECT * FROM user_channel").fetchall()
+    flack = "Flack"
+    channel_decription = "This room is flack official public room"
+    return render_template("chatroom.html", flack=flack, user_id=session['user_id'], user_name=session['username'],
+                           channels=channels, channel_decription=channel_decription)
+
 
 @app.route("/channels/<int:channel_id>")
 @login_required
 def channel(channel_id):
-	# Make sure channel exists.
-	channel = db.execute("SELECT * FROM user_channel WHERE id = :id", {"id": channel_id}).fetchone()
-	if channel is None:
-		return "No such channel."
-	# I'm using ''.join here because query return a tuple
-	channel_name =''.join(db.execute("SELECT channel FROM user_channel WHERE id = :id", {"id": channel_id}).fetchone())
-	channel_decription =''.join(db.execute("SELECT description FROM user_channel WHERE id = :id", {"id": channel_id}).fetchone())
-	channels =channels = db.execute("SELECT * FROM user_channel").fetchall()	
-	return render_template("chatroom.html",user_id=session['user_id'],user_name=session['username'], channel_name=channel_name,channels=channels,channel_decription=channel_decription)    
+    # In idle database loses its connection and should has been refreshed
+    setup_database()
+    # Make sure channel exists.
+    channel = channel_dao.findChannelByChannelID(channel_id)
+    # channel = db.execute("SELECT * FROM user_channel WHERE id = :id", {"id": channel_id}).fetchone()
+    if channel is None:
+        return "No such channel."
+    channel_name = channel.channel
+    channel_decription = channel.description
+    channels = channel_dao.findall()
+    # channel = channels = db.execute("SELECT * FROM user_channel").fetchall()
+    return render_template("chatroom.html", user_id=session['user_id'], user_name=session['username'],
+                           channel_name=channel_name, channels=channels, channel_decription=channel_decription)
+
 
 @socketio.on("entry message")
 def message(data):
-	message = data['message']
-	name = data['name']
-	room = data['rooma']
-	from datetime import datetime
-	now = datetime.now()
-	time = now.strftime("%I:%M:%S")
-	join_room(room)
-	emit("announce message", {"message": message,"name":name,"time":time}, room=room, broadcast=True)
-	
+    message = data['message']
+    name = data['name']
+    room = data['rooma']
+    import time
+    message_time = int(round(time.time() * 1000))
+    join_room(room)
+    emit("announce message", {"message": message, "name": name, "time": message_time}, room=room, broadcast=True)
+
+
 @socketio.on("submit message")
 def message(data):
-	message = data['message']
-	name = data['name']
-	room = data['rooma']
-	from datetime import datetime
-	now = datetime.now()
-	time = now.strftime("%I:%M:%S")
-	join_room(room)
-	emit("announce message", {"message": message,"name":name,"time":time}, room=room, broadcast=True)
+    message = data['message']
+    name = data['name']
+    room = data['rooma']
+    import time
+    message_time = int(round(time.time() * 1000))
+    join_room(room)
+    emit("announce message", {"message": message, "name": name, "time": message_time}, room=room, broadcast=True)
+
 
 if __name__ == '__main__':
-	socketio.run(app)
+    # app.debug = True
+    socketio.run(app)
